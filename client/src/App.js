7853f74f45cee595fced4119d303ec8121c78257
muditagarwalna@gmail.comx <muditagarwalna@gmail.com>
2022-11-06 00:41:41 +0530

    Added Routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,26 @@
-import './App.css';
-import Navbar from "./components/Navbar";
-
-import React from 'react'
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+// import Navbar from "./components/Navbar";
+import ShopPage from "./pages/ShopPage";
+import React from "react";
+import AdminProductsPage from "./pages/AdminProductsPage";
+import AddProductsPage from "./pages/AddProductsPage";
+import CartPage from "./pages/CartPage";
+import OrdersPage from "./pages/OrdersPage";
+import ProductsPage from "./pages/ProductsPage";
 
 const App = () => {
   return (
-    <div>
-      <Navbar/>
-    </div>
-  )
-}
+    <Routes>
+      <Route path="/" element={<ShopPage />} />
+      <Route path="/products" element={<ProductsPage />} />
+      <Route path="/cart" element={<CartPage />} />
+      <Route path="/orders" element={<OrdersPage />} />
+      <Route path="/admin/add-product" element={<AddProductsPage />} />
+      <Route path="/admin/admin-products" element={<AdminProductsPage />} />
+      {/* <Navbar /> */}
+    </Routes>
+  );
+};
 
 export default App;
