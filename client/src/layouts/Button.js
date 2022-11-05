ba4244ef9f91f66bf990a2e764ab50244afde525
muditagarwalna@gmail.comx <muditagarwalna@gmail.com>
2022-11-06 00:43:18 +0530

    Created Button

--- client/src/layouts/Button.js
+++ client/src/layouts/Button.js
@@ -1,11 +1,22 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import classes from "./Button.module.css";
 
 const Button = (props) => {
+  const [btnClassName, setBtnClassName] = useState(props.className);
+
+  useEffect(() => {
+    if (props.className !== "") {
+      setBtnClassName(props.className);
+    } else {
+      setBtnClassName(classes.btn);
+    }
+  }, [props.className]);
+
+  console.log(props.className);
   return (
     <div>
-      <button className={`${props.className} ${classes.btn}`} type={props.type}>
-        {props.data}
+      <button className={btnClassName} type={props.type}>
+        {props.children}
       </button>
     </div>
   );
