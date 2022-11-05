430e4eaafd9e03e0d7792bb2347a734ff71d1070
muditagarwalna@gmail.comx <muditagarwalna@gmail.com>
2022-11-06 00:46:39 +0530

    Created Product schema

--- server/models/products.js
+++ server/models/products.js
@@ -15,8 +15,10 @@ const productSchema = new Schema({
     type: String,
     required: true,
   },
-  imageUrl: {
+  image: {
     type: String,
     required: true,
   },
 });
+
+module.exports = mongoose.model("Product", productSchema);
