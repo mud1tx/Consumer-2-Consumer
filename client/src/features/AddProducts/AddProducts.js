ab2184e3d591134e28062a47dcaa89806b9fc67c
muditagarwalna@gmail.comx <muditagarwalna@gmail.com>
2022-11-09 16:07:02 +0530

    created complete Add Product form

--- client/src/features/AddProducts/AddProducts.js
+++ client/src/features/AddProducts/AddProducts.js
@@ -1,16 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "../../components/Navbar";
 import classes from "./AddProducts.module.css";
+import ImagePond from "../../components/ImagePond";
 
 const AddProducts = () => {
+  const [user, setUser] = useState({ title: "",category:"", price: "", description: "" });
+  const handleInputs = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    setUser({ ...user, [name]: value });
+    // console.log(user);
+  };
+
   return (
     <div>
       <Navbar />
       <main className={classes.addProduct_form}>
         <form
           className={classes.product_form}
-          // action="/admin/add-product"
-          // method="post"
+          action="/admin/add-product"
+          method="POST"
         >
           <div className={classes.form_control}>
             <label htmlFor="title">Title</label>
@@ -18,13 +27,23 @@ const AddProducts = () => {
               type="text"
               name="title"
               id="title"
-              // value={user.title}
-              // onChange={handleInputs}
+              value={user.title}
+              onChange={handleInputs}
             />
           </div>
           <div className={classes.form_control}>
-            <label htmlFor="actual-btn">Choose File</label>
-            <input type="file" id="actual-btn" name="imageUrl" />
+            <label htmlFor="category">Category</label>
+            <input
+              type="text"
+              name="category"
+              id="category"
+              value={user.category}
+              onChange={handleInputs}
+            />
+          </div>
+          <div className={classes.form_control}>
+            <label htmlFor="image">Choose File</label>
+            <ImagePond name="image" />
           </div>
           <div className={classes.form_control}>
             <label htmlFor="price">Price</label>
@@ -32,8 +51,8 @@ const AddProducts = () => {
               type="number"
               name="price"
               id="price"
-              // value={user.price}
-              // onChange={handleInputs}
+              value={user.price}
+              onChange={handleInputs}
             />
           </div>
           <div className={classes.form_control}>
@@ -42,14 +61,13 @@ const AddProducts = () => {
               name="description"
               id="description"
               rows="5"
-              // value={user.description}
-              // onChange={handleInputs}
+              value={user.description}
+              onChange={handleInputs}
             ></textarea>
           </div>
           <button
             className={classes.addProduct_form_submit_btn}
             type="submit"
-            onClick={() => {}}
           >
             Add-Product
           </button>
