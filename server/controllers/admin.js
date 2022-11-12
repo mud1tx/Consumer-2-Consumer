a083abc7e1a98de6d621c3c661a1c3e1815380e5
muditagarwalna@gmail.comx <muditagarwalna@gmail.com>
2022-11-12 14:12:30 +0530

    addProduct model and connection

--- server/controllers/admin.js
+++ server/controllers/admin.js
@@ -1,72 +1,69 @@
 const Product = require("../models/products");
+const fs = require("fs");
 const imageMimeTypes = ["image/jpeg", "image/png", "image/gif"];
 
 exports.postAddProduct = async (req, res, next) => {
-  // console.log(req.body.image.length);
-
   const title = req.body.title;
   const category = req.body.category;
-  const image = req.body.image;
+  const images = req.files;
   const price = req.body.price;
   const description = req.body.description;
+  if (!images) {
+    const error = new Error("Please choose files");
+    error.httpStatusCode = 400;
+    return next(error);
+  }
+  // conver images into base64 encoding
+  let imgArray = images.map((file) => {
+    let img = fs.readFileSync(file.path);
+    return (en = img.toString("base64"));
+  });
+  const imageArray = [];
+  const imageTypeArray = [];
+  const imageNameArray = [];
+  imgArray.map((src, index) => {
+    imageNameArray.push(images[index].originalname);
+    imageTypeArray.push(images[index].mimetype);
+    imageArray.push(src);
+  });
   const product = new Product({
     title: title,
     category: category,
-    image: image,
+    imageName: imageNameArray,
+    imageType: imageTypeArray,
+    image: imageArray,
     price: price,
     description: description,
+    userId:req.user._id
   });
-  saveImage(product, image);
-  try {
-    const newProduct = await product.save();
-    res.redirect("/");
-  } catch (error) {
-    console.log(error);
-  }
-  // product
-  //   .save()
-  //   .then((result) => {
-  //     console.log("Created Product");
-  //     res.redirect("/");
-  //   })
-  //   .catch((err) => {
-  //     console.log(err);
-  //   });
+  product
+    .save()
+    .then((result) => {
+      console.log("Created Product");
+      res.redirect("/");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+  // res.json(product);
 };
 
+
 // function saveImage(product, imgEncoded) {
-//   if (imgEncoded == null) {
+//   if (imgEncoded === null) {
 //     return;
 //   }
-//   const img = JSON.parse(imgEncoded);
-//   if (img != null && imageMimeTypes.includes(img.type)) {
-//     product.image = new Buffer.from(img.data, "base64");
-//     product.imageType = img.type;
+//   if (!Array.isArray(imgEncoded)) {
+//     const singleImg = JSON.parse(imgEncoded);
+//     product.image.push = new Buffer.from(singleImg.data, "base64");
+//     product.imageType.push(singleImg.type);
+//   } else {
+//     for (let i = 0; i < imgEncoded.length; i++) {
+//       const img = JSON.parse(imgEncoded[i]);
+//       if (img != null && imageMimeTypes.includes(img.type)) {
+//         product.image.push = new Buffer.from(img.data, "base64");
+//         product.imageType.push(img.type);
+//       }
+//     }
 //   }
 // }
-
-function saveImage(product, imgEncoded) {
-  // const imageTypeArray = [];
-  // const imageArray = [];
-  if (imgEncoded == null) {
-    return;
-  }
-  // console.log(imgEncoded);
-  // console.log(imgEncoded);
-  // console.log(ima);
-  for (let i = 0; i < imgEncoded.length; i++) {
-    // if(imgEncoded.length===1){
-
-    // }
-    const img = JSON.parse(imgEncoded[i]);
-    console.log(img);
-    if (img != null && imageMimeTypes.includes(img.type)) {
-      product.image.push = new Buffer.from(img.data, "base64");
-      product.imageType.push(img.type);
-    }
-    // console.log(JSON.parse(imgEncoded[i]));
-  }
-  // product.image = imageArray;
-  // product.imageType = imageTypeArray;
-  // console.log(image.length);
-}
--- server/models/products.js
+++ server/models/products.js
@@ -11,12 +11,15 @@ const productSchema = new Schema({
     type: String,
     required: true,
   },
-  image: {
-    // type: Buffer,
-    type: [Buffer],
+  imageName: {
+    type: [String],
     required: true,
   },
   imageType: {
+    type: [String],
+    required: true,
+  },
+  image:{
     type: [String],
     required: true,
   },
@@ -28,6 +31,31 @@ const productSchema = new Schema({
     type: String,
     required: true,
   },
+  userId:{
+    type:Schema.Types.ObjectId,
+    ref:'User',
+    required: true,
+  }
 });
+
+// productSchema.virtual('imgSrc').get(function(){
+//   if(this.image!=null && this.imageType!=null){
+//     return `data:${this.imageType}:charSet=utf-8;base64,${this.image.toString('base64')}`
+//   }
+// })
+
+// productSchema.virtual("imgSrc").get(function () {
+//   const imageSrcData = [];
+//   for (let i = 0; i < this.image.length; i++) {
+//     if (this.image[i] != null && this.imageType[i] != null) {
+//       imageSrcData.push(
+//         `data:${this.imageType[i]}:charSet=utf-8;base64,${this.image[
+//           i
+//         ].toString("base64")}`
+//       );
+//     }
+//   }
+//   return imageSrcData;
+// });
 
 module.exports = mongoose.model("Product", productSchema);
