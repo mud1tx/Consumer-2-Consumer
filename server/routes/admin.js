a1ea9cd89a7930e0bff3ba6d14f25449494f1fa4
muditagarwalna@gmail.comx <muditagarwalna@gmail.com>
2022-11-12 14:18:33 +0530

    admin route

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -2,10 +2,16 @@ const express = require("express");
 
 const adminController = require("../controllers/admin");
 
+const store = require("../util/multer");
+
 const router = express.Router();
 
 // router.get("/add-product", adminController.getAddProduct);
 
-router.post('/add-product', adminController.postAddProduct);
+router.post(
+  "/add-product",
+  store.array("images",12),
+  adminController.postAddProduct
+);
 
 module.exports = router;
