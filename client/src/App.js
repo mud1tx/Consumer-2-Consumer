b10353cf1c5f54b10977e2e9ead6c0ee6a51b485
muditagarwalna@gmail.comx <muditagarwalna@gmail.com>
2022-11-12 15:37:29 +0530

    remove app.css file

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,4 @@
-import "./App.css";
 import { Routes, Route } from "react-router-dom";
-// import Navbar from "./components/Navbar";
 import ShopPage from "./pages/ShopPage";
 import React from "react";
 import AdminProductsPage from "./pages/AdminProductsPage";
