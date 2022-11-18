74aacbf57b9ea4ecc17244c343efef68ac6b0c35
muditagarwalna@gmail.comx <muditagarwalna@gmail.com>
2022-11-18 16:07:34 +0530

    added route protection

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -4,13 +4,16 @@ const adminController = require("../controllers/admin");
 
 const store = require("../util/multer");
 
+const isAuth = require("../middleware/is_auth");
+
 const router = express.Router();
 
 // router.get("/add-product", adminController.getAddProduct);
 
 router.post(
   "/add-product",
-  store.array("images",12),
+  store.array("images", 12),
+  isAuth,
   adminController.postAddProduct
 );
 
