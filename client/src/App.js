dd77d48151f040b35f758924206676c7358c4f0d
muditagarwalna@gmail.comx <muditagarwalna@gmail.com>
2022-11-23 00:09:34 +0530

    added privateRoutes and sessionstorage logic

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 import ShopPage from "./pages/ShopPage";
-import React from "react";
+import React, { useEffect } from "react";
 import AdminProductsPage from "./pages/AdminProductsPage";
 import AddProductsPage from "./pages/AddProductsPage";
 import CartPage from "./pages/CartPage";
@@ -9,18 +9,32 @@ import ProductsPage from "./pages/ProductsPage";
 import Login from "./features/Authentication/Login";
 import Signup from "./features/Authentication/Signup";
 import { UserContextProvider } from "./context/UserContext";
-import "react-toastify/dist/ReactToastify.css";
+import PrivateRoute from "./features/PrivateRoutes/PrivateRoutes";
+import { User } from "./redux/action/authUser";
+import { useDispatch } from "react-redux";
 
 const App = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const loggedInUser = sessionStorage.getItem("userLoggedIn");
+    if (loggedInUser) {
+      const foundUser = JSON.parse(loggedInUser);
+      dispatch(User(foundUser));
+    }
+  });
+
   return (
     <UserContextProvider>
       <Routes>
         <Route path="/" element={<ShopPage />} />
-        <Route path="/products" element={<ProductsPage />} />
-        <Route path="/cart" element={<CartPage />} />
-        <Route path="/orders" element={<OrdersPage />} />
-        <Route path="/admin/add-product" element={<AddProductsPage />} />
-        <Route path="/admin/admin-products" element={<AdminProductsPage />} />
+        <Route element={<PrivateRoute />}>
+          <Route path="/products" element={<ProductsPage />} />
+          <Route path="/cart" element={<CartPage />} />
+          <Route path="/orders" element={<OrdersPage />} />
+          <Route path="/admin/add-product" element={<AddProductsPage />} />
+          <Route path="/admin/admin-products" element={<AdminProductsPage />} />
+        </Route>
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         {/* <Navbar /> */}
