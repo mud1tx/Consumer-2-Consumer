8b6a6e57faa04a041c351bd96fc3c1ac7410ddde
muditagarwalna@gmail.comx <muditagarwalna@gmail.com>
2022-11-26 23:57:42 +0530

    changed user reducer logic

--- client/src/redux/reducer/authUserReducer.js
+++ client/src/redux/reducer/authUserReducer.js
@@ -1,13 +1,12 @@
-const initialState = {};
+const initial_state = {
+  user: "",
+};
 
-export const authenticateUser = (state = initialState, { type, payload }) => {
+export const authenticateUser = (state = initial_state, { type, payload }) => {
   switch (type) {
     case "AUTH_USER":
-      // console.log("payload",payload);
-      state = { ...payload };
-      console.log("state", state);
-      return state;
+      return { ...state, user: payload };
     default:
-      return null;
+      return state;
   }
 };
