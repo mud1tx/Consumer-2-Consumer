7397403d6ed46224e7d66c9ba5855387a426835a
muditagarwalna@gmail.comx <muditagarwalna@gmail.com>
2022-11-26 23:56:59 +0530

    added product detail reducer

--- client/src/redux/reducer/index.js
+++ client/src/redux/reducer/index.js
@@ -1,9 +1,10 @@
 import { combineReducers } from "redux";
-
 import { authenticateUser } from "./authUserReducer";
+import { productDetail } from "./productDetailReducer";
 
 const reducers = combineReducers({
   authenticateUser,
+  productDetail,
 });
 
 export default reducers;
