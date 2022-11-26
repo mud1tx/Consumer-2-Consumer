bf8d9281fb8dc29f9de202ce81990d928b39afe8
muditagarwalna@gmail.comx <muditagarwalna@gmail.com>
2022-11-27 00:04:38 +0530

    added getsingleProduct Detail logic

--- server/routes/shop.js
+++ server/routes/shop.js
@@ -1,10 +1,11 @@
 const express = require("express");
 
 const shopController = require("../controllers/shop");
-// const isAuth = require("../middleware/is-auth");
 
 const router = express.Router();
 
 router.get("/", shopController.getProducts);
+
+router.get("/:productId", shopController.getProduct);
 
 module.exports = router;
