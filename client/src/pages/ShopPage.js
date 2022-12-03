9e3d8f2f11848338a28093ab29d6e59842ba07c6
muditagarwalna@gmail.comx <muditagarwalna@gmail.com>
2022-12-03 11:49:04 +0530

    remove homepage during searching

--- client/src/pages/ShopPage.js
+++ client/src/pages/ShopPage.js
@@ -1,13 +1,21 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Shop from "../features/Shop/Shop";
 import Navbar from "../components/Navbar";
 import Hero from "../features/Shop/Hero";
+import { useSelector } from "react-redux";
 
 const ShopPage = () => {
+  const searchData = useSelector((state) => state.searchInputReducer);
+
+  const [searchProduct, setSearchProduct] = useState(searchData);
+
+  useEffect(() => {
+    setSearchProduct(searchData);
+  }, [searchData]);
   return (
     <div className="bg-main_color-200">
       <Navbar />
-      <Hero />
+      {searchProduct === "" ? <Hero /> : <></>}
       <div>
         <h1 className="text-3xl font-semibold text-main_color-1000 ml-4">
           Featured Product SoFar
