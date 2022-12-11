629280a4137151c9c147d116d268f42003184494
muditagarwalna@gmail.comx <muditagarwalna@gmail.com>
2022-12-11 15:25:50 +0530

    added message model for mongoDB

---
+++ server/models/message.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const messageSchema = new Schema(
+  {
+    conversationId: {
+      type: String,
+    },
+    senderId: {
+      type: String,
+    },
+    text: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Message", messageSchema);
