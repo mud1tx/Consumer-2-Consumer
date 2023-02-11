98cd7dc5dac6ec571b904a1abe00871f6d912498
muditagarwalna@gmail.comx <muditagarwalna@gmail.com>
2023-02-11 20:45:28 +0530

    change full logic of add product using multer

--- server/util/multer.js
+++ server/util/multer.js
@@ -3,16 +3,13 @@ const multer = require("multer");
 //set storage
 var storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    console.log("dfgsfg", file);
     cb(null, "./server/uploads");
   },
   filename: function (req, file, cb) {
-    console.log("asdjasndkjasndkjankdjansjkdjnas", file);
     var ext = file.originalname.substr(file.originalname.lastIndexOf("."));
     cb(null, file.fieldname + "-" + Date.now() + ext);
   },
 });
 
 const store = multer({ storage: storage });
-console.log("erggdrgdgdrgd", store);
 module.exports = store;
