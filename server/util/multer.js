3d7a51ef68710db69b2cf0b85f89a6e3a488cb28
muditagarwalna@gmail.comx <muditagarwalna@gmail.com>
2023-02-13 01:47:24 +0530

    change multer logic

--- server/util/multer.js
+++ server/util/multer.js
@@ -1,15 +1,18 @@
 const multer = require("multer");
 
 //set storage
-var storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "./server/uploads");
-  },
-  filename: function (req, file, cb) {
-    var ext = file.originalname.substr(file.originalname.lastIndexOf("."));
-    cb(null, file.fieldname + "-" + Date.now() + ext);
-  },
-});
+// var storage = multer.diskStorage({
+//   destination: function (req, file, cb) {
+//     cb(null, "./server/uploads");
+//   },
+//   filename: function (req, file, cb) {
+//     var ext = file.originalname.substr(file.originalname.lastIndexOf("."));
+//     cb(null, file.fieldname + "-" + Date.now() + ext);
+//   },
+// });
 
-const store = multer({ storage: storage });
-module.exports = store;
+// const store = multer({ storage: storage });
+const storage = multer.memoryStorage();
+const upload = multer({ storage }).array("images");
+module.exports = upload;
+
